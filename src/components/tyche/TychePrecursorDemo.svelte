5ca19b6beeeba1bb156841316b582b68919e1c38
lumirth <65358837+lumirth@users.noreply.github.com>
2023-04-14 07:16:29 -0500

    make tyche precursor pass google lighthouse

--- src/components/tyche/TychePrecursorDemo.svelte
+++ src/components/tyche/TychePrecursorDemo.svelte
@@ -1,38 +1,40 @@
 <!-- DiceExpression.svelte -->
 <script lang="ts">
-    import { evaluateDiceExpression } from './diceExpression';
-    let expression = '';
-    let result = '';
-    let history = '';
-  
-    function onSubmit() {
-      if (expression === '' || expression === undefined) {
-        history = 'Please enter a dice expression';
-        return;
-      }
-      try {
-        [result, history] = evaluateDiceExpression(expression);
-        console.log(result, history)
-      } catch (error) {
-        history = error.message;
-      }
+  import { evaluateDiceExpression } from './diceExpression';
+  let expression = '';
+  let result = '';
+  let history = '';
+
+  function onSubmit() {
+    if (expression === '' || expression === undefined) {
+      history = 'Please enter a dice expression';
+      return;
     }
-  </script>
-  <div class="blockquote">
-    <form on:submit|preventDefault={onSubmit}>
-      <h1>Try it out!</h1>
-      <!-- <hr class="hori-line"> -->
-      <div class="input-and-buttons-container" style="align-items: left; justify-content:flex-start;">
-        <input class="input-text-line" type="text" id="expression" bind:value={expression} />
-        <button class="button" type="submit">Evaluate</button>
-      </div>
-      <br />
-    </form>
-    {#if result}
-    <h2>Result: {result}</h2>
-    {/if}
-    {#if history}
-    <pre class="code--block">{history}</pre>
-    {/if}
-  </div>
-    
+    try {
+      [result, history] = evaluateDiceExpression(expression);
+      console.log(result, history)
+    } catch (error) {
+      history = error.message;
+    }
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      onSubmit();
+    }
+  }
+</script>
+<div class="blockquote">
+    <h1>Try it out!</h1>
+    <div class="input-and-buttons-container" style="align-items: left; justify-content:flex-start;">
+      <input class="input-text-line" aria-label="Enter dice roll expression" type="text" id="expression" bind:value={expression} on:keydown={handleKeyDown} />
+      <button class="button" type="button" on:click={onSubmit}>Evaluate</button>
+    </div>
+    <br />
+  {#if result}
+  <h2>Result: {result}</h2>
+  {/if}
+  {#if history}
+  <pre class="code--block">{history}</pre>
+  {/if}
+</div>
