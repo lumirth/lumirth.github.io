8f1659feb101e2d678a492d716d90665a564b9b3
lumirth <65358837+lumirth@users.noreply.github.com>
2023-06-21 12:40:43 -0500

    move page titles to consts

--- src/consts.ts
+++ src/consts.ts
@@ -6,3 +6,8 @@ export const SITE_DESCRIPTION = 'Welcome to my website!';
 export const AUTHOR_NAME = 'Lukas Unguraitis';
 export const AUTHOR_FIRST_NAME = 'Lukas';
 export const AUTHOR_LAST_NAME = 'Unguraitis';
+
+export const SLASH = 'LUMIRTH - Lukas Unguraitis';
+export const SLASH_BLOG= 'LUMIRTH - BLOG';
+export const SLASH_WORK = 'LUMIRTH - WORK';
+export const SLASH_INFO = 'LUMIRTH - INFO';
